c2c019237eacbf3a101c0b62f5768efa1751595f
Sivert Sivertsen <sivertlv3@gmail.com>
2026-01-22 23:48:07 +0100

    headers an shi

--- xv6/user/ps.c
+++ xv6/user/ps.c
@@ -6,11 +6,20 @@
 
 int main(){
 
-    //format -> proc name(proc id): proc state
+    struct pInfo procs[64];
+    int count = getprocs(procs);
+
+    for (int i = 0; i < count; i++)
+    {
+        printf(1, "PID: %d | Name: %s | State: %d\n", procs[i].name, procs[i].PID, procs[i].state);
+    }
     
-    acquire(&ptable.lock);
-
-
 
     exit(0);
 }
+
+struct pInfo{
+    int PID;
+    int state;
+    char name[16];
+};
---
+++ xv6/user/ps.d
@@ -0,0 +1,2 @@
+user/ps.o: user/ps.c user/user.h kernel/types.h kernel/stat.h \
+ kernel/proc.h
